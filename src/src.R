b062c965242f37bf1fb14bcd94987c6f7068b662
AlejoAcelas <sergio.yo007@gmail.com>
2021-11-02 08:24:01 -0500

    Avancé en src

--- src/src.R
+++ src/src.R
@@ -3,25 +3,52 @@ library(rio)
 library(tidyverse)
 library(skimr)
 
+rm(list = ls())
+
 chip = list()
 anos = 2017:2020
 names = list()
+
 for (a in anos){
-  i = 0
   for (file in list.files(paste0('./data/input/', a) , '.xls', full.names = T)){
+    # Leo la celda con el nombre y el identificador
     id = import(file, col_names = F, range="A3:A3")
     
+    # Guardo el nombre del municipio y el código por separado
+    # El [[1]] es solo porque el output está como lista, por lo que extraigo el elemento
     id = str_split(id[[1]], " - ", 2)[[1]]
+    # Guardo el resultado en una lista
     names[[id[1]]] = id[2]
     
+    # Importo la base de datos
     df = import(file, col_names = T, skip = 9)
-    chip[[paste0(a, '_', id[1])]] = df
-    i = i + 1
+    
+    # Miro cuantas veces aparece el mismo municiipio para el mismo año
+    nombre = paste0(a, '_', id[1])
+    ocurrencias_pasadas = length(grep(nombre, names(chip)))
+    # Si es distinto de 0 cambio el nombre para no reemplazar los datos de antes
+    if (ocurrencias_pasadas != 0){
+      nombre = paste0(nombre, "_", ocurrencias_pasadas)
+    }
+    
+    # Agrego df a chip. El nombre es el año y el código identificador
+    chip[[nombre]] = df
   }
 }
 
-df = chip[[1]]
-skim(df)
+pago_educacion = function(df){
+  pago = df %>% filter(CODIGO == "A.1") %>% select("PAGOS(Pesos)")
+  pago = gsub(",", ".", pago)
+  print(pago)
+  return(pago %>% as.double())
+}
 
-a = import("./data/input/2020/21000540045K212410-1220201625674324667.xls", col_names = F, range="A3:A3")
-t = a[[1]] %>% strsplit(" - ")
+pagos = lapply(chip, pago_educacion)
+
+# Hay un outlier
+Pagos_Educacion = unlist(pagos)
+hist(Pagos_Educacion)
+
+# No hay lío, es solo Medellín
+names["210105001"]
+hist(log10(Pagos_Educacion), breaks=15)
