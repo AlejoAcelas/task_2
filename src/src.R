982572c21a0a8f7505e987ea8e0a4f544e7974cd
Angelica Maria <a.alvarezb@uniandes.edu.co>
2021-11-12 21:23:06 -0500

    cleanup y avance punto 2

--- src/src.R
+++ src/src.R
@@ -9,6 +9,10 @@ chip = list()
 anos = 2017:2020
 names = list()
 
+################
+#   PUNTO 1    #
+################
+
 for (a in anos){
   for (file in list.files(paste0('./data/input/', a) , '.xls', full.names = T)){
     # Leo la celda con el nombre y el identificador
@@ -36,29 +40,52 @@ for (a in anos){
   }
 }
 
-<<<<<<< HEAD
-pagos_educacion= function(df) {
-  pago=df %>% if (CODIGO=="A.1"){select("PAGOS(Pesos)")}
   
-  
-  
-  
+
+################
+#   PUNTO 2    #PROPUESTA
+################
+
+rm(list=ls())
+require(pacman)
+p_load(rio,tidyverse)
+
+# punto 1
+paths = lapply(2017:2020 , function(x) list.files(paste0("./data/input/",x),full.names=T)) %>%
+  unlist()
+
+list_chip = list()
+for (i in 1:length(paths)){
+  list_chip[[i]] = import(file = paths[i])  
 }
 
+pago_educ = function(n,lista,tipo_rubro){
+  lista_n = lista[[n]] 
+  colnames(lista_n) = lista_n[7,]
+  valor = lista_n %>% subset(NOMBRE==tipo_rubro) %>% select(`PAGOS(Pesos)`)
+  return(valor)  
+}
 
+pago_educ = function(n,lista,tipo_rubro){
+  
+  # crear df
+  df = data.frame(valor=NA,cod_dane=NA,periodo=NA)  
+  lista_n = lista[[n]] 
+  
+  # extraer codigo dane
+  df$cod_dane = colnames(lista_n)[1]
+  
+  # extraer periodo
+  df$periodo = lista_n[2,1]
+  
+  # extraer el valor
+  colnames(lista_n) = lista_n[7,]
+  df$valor = lista_n %>% subset(NOMBRE==tipo_rubro) %>% select(`PAGOS(Pesos)`)
+  
+  return(df)  
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+pago_educ(n = 10 , lista = list_chip , tipo_rubro = "EDUCACIÓN")
 
 
 
@@ -71,11 +98,14 @@ pago_educacion = function(df){
   return(pago %>% as.double())
 }
 
+################
+#   PUNTO 3    #PROPUESTA
+################
+
 pagos = lapply(chip, pago_educacion)
 
 # Hay un outlier
 Pagos_Educacion = unlist(pagos)
 hist(Pagos_Educacion)
-=======
->>>>>>> 786a9f242fcf9abe087cc9ac419a60432413fd15
+
 
