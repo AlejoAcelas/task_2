85edf0ecd766fd97d8b96b9d184edd85ccbd6831
AlejoAcelas <sergio.yo007@gmail.com>
2021-11-14 18:53:11 -0500

    Añadi el periodo a cada df

--- src/src.R
+++ src/src.R
@@ -21,11 +21,19 @@ for (a in anos){
     # Guardo el nombre del municipio y el código por separado
     # El [[1]] es solo porque el output está como lista, por lo que extraigo el elemento
     id = str_split(id[[1]], " - ", 2)[[1]]
-    # Guardo el resultado en una lista
+    # Guardo el resultado en una lista (no lo uso despues, pero fue util para explorar)
     names[[id[1]]] = id[2]
     
     # Importo la base de datos
     df = import(file, col_names = T, skip = 9)
+    
+    # Importo el periodo
+    periodo = import(file, col_names = F, range="A5:A5")
+
+    # Agrego varios datos como columnas para facilitarme la vida
+    df$periodo = periodo[[1]]
+    df$municipio = id[2]
+    df$cod_dane = id[1]
     
     # Miro cuantas veces aparece el mismo municiipio para el mismo año
     nombre = paste0(a, '_', id[1])
@@ -45,26 +53,6 @@ for (a in anos){
 ################
 #   PUNTO 2    #PROPUESTA
 ################
-
-rm(list=ls())
-require(pacman)
-p_load(rio,tidyverse)
-
-# punto 1
-paths = lapply(2017:2020 , function(x) list.files(paste0("./data/input/",x),full.names=T)) %>%
-  unlist()
-
-list_chip = list()
-for (i in 1:length(paths)){
-  list_chip[[i]] = import(file = paths[i])  
-}
-
-pago_educ = function(n,lista,tipo_rubro){
-  lista_n = lista[[n]] 
-  colnames(lista_n) = lista_n[7,]
-  valor = lista_n %>% subset(NOMBRE==tipo_rubro) %>% select(`PAGOS(Pesos)`)
-  return(valor)  
-}
 
 pago_educ = function(n,lista,tipo_rubro){
   
@@ -92,10 +80,9 @@ pago_educ(n = 10 , lista = list_chip , tipo_rubro = "EDUCACIÓN")
 
 
 pago_educacion = function(df){
-  pago = df %>% filter(CODIGO == "A.1") %>% select("PAGOS(Pesos)")
-  pago = gsub(",", ".", pago)
-  print(pago)
-  return(pago %>% as.double())
+  pago = df %>% filter(CODIGO == "A.1") %>% 
+    select("PAGOS(Pesos)", "periodo", "cod_dane")
+  return(pago)
 }
 
 ################
@@ -104,8 +91,22 @@ pago_educacion = function(df){
 
 pagos = lapply(chip, pago_educacion)
 
-# Hay un outlier
-Pagos_Educacion = unlist(pagos)
-hist(Pagos_Educacion)
 
+
+
+# punto 1
+paths = lapply(2017:2020 , function(x) list.files(paste0("./data/input/",x),full.names=T)) %>%
+  unlist()
+
+list_chip = list()
+for (i in 1:length(paths)){
+  list_chip[[i]] = import(file = paths[i])  
+}
+
+pago_educ = function(n,lista,tipo_rubro){
+  lista_n = lista[[n]] 
+  colnames(lista_n) = lista_n[7,]
+  valor = lista_n %>% subset(NOMBRE==tipo_rubro) %>% select(`PAGOS(Pesos)`)
+  return(valor)  
+}
 
